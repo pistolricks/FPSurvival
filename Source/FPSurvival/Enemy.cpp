16218974dd0aba1072895a4d36d0ff7132a762ab
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-07-30 04:14:36 -0700

    Add `AEnemy` class for enemy AI behavior
    - Created `Enemy.h` and `Enemy.cpp` for the basic enemy character.
    - Implemented `SeekPlayer` functionality to continuously move towards the player.
    - Introduced references for `PlayerREF`, `EnemyAIC`, and `StoppingDistance`.

---
+++ Source/FPSurvival/Enemy.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy.h"
+#include "FPSurvivalCharacter.h"
+#include "AIController.h"
+#include "Navigation/PathFollowingComponent.h"
+
+// Sets default values
+AEnemy::AEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	EnemyAIC = Cast<AAIController>(GetController());
+
+	SeekPlayer();	
+}
+
+// Called every frame
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AEnemy::SeekPlayer()
+{
+	GetWorld()->GetTimerManager().SetTimer(SeekPlayerTH, this, &AEnemy::SeekPlayer, 0.25f, true);
+
+	if (PlayerREF)
+	{
+		EnemyAIC->MoveToLocation(PlayerREF->GetActorLocation(), StoppingDistance,true);
+	}
+	else
+	{
+		PlayerREF = Cast<AFPSurvivalCharacter>(GetWorld()->GetFirstPlayerController()->GetCharacter());
+		SeekPlayer();
+	}
+}
+
