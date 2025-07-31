9d27ac14ab245a994387d011bf39c7a913b3193b
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-07-31 01:21:42 -0700

    Add `AFPSurvivalProjectile` class for projectile handling
    - Introduced `AFPSurvivalProjectile` class to manage projectile logic, including collision handling and movement using `USphereComponent` and `UProjectileMovementComponent`.
    - Implemented `OnHit` function to apply damage to hit actors and destroy the projectile.
    - Added `damage` and `DamageType` properties for customizing projectile damage behavior.

---
+++ Source/FPSurvival/FPSurvivalProjectile.cpp
@@ -0,0 +1,41 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FPSurvivalProjectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/SphereComponent.h"
+#include <Kismet/GameplayStatics.h>
+
+AFPSurvivalProjectile::AFPSurvivalProjectile() 
+{
+	// Use a sphere as a simple collision representation
+	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	CollisionComp->InitSphereRadius(5.0f);
+	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
+	CollisionComp->OnComponentHit.AddDynamic(this, &AFPSurvivalProjectile::OnHit);		// set up a notification for when this component hits something blocking
+
+	// Players can't walk on it
+	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
+	CollisionComp->CanCharacterStepUpOn = ECB_No;
+
+	// Set as root component
+	RootComponent = CollisionComp;
+
+	// Use a ProjectileMovementComponent to govern this projectile's movement
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
+	ProjectileMovement->UpdatedComponent = CollisionComp;
+	ProjectileMovement->InitialSpeed = 3000.f;
+	ProjectileMovement->MaxSpeed = 3000.f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = true;
+
+	// Die after 3 seconds by default
+	InitialLifeSpan = 3.0f;
+}
+
+void AFPSurvivalProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	AController* PlayerC = GetInstigatorController();
+
+	UGameplayStatics::ApplyDamage(OtherActor, damage, PlayerC, this, DamageType);
+	Destroy();
+}
